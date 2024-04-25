a3577ec715ba0e73491a5bf5294b8c52db91c69b
redha-boubakour <redha.boubakour@yahoo.fr>
2024-04-25 20:30:03 +0200

    commit sidebar

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/profile, /credits"]);
+const isProtectedRoute = createRouteMatcher(["/profile"]);
 
 export default clerkMiddleware((auth, req) => {
     if (isProtectedRoute(req)) auth().protect();
